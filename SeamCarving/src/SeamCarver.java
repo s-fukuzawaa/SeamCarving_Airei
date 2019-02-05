a5172ad8419a9a70e4e8070549dfe436fd8de4d6
s-fukuzawaa <31524675+s-fukuzawaa@users.noreply.github.com>
2019-02-05 09:31:34 -0800

    verti tests failied. following mr broman's advice, tried modify prev so that source is prior to target. made a node class to indicate. update prev to 2d node. worked for verti. not for hori.

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class SeamCarver
 {
 	private SmC_Picture pic;
@@ -49,33 +50,48 @@ public class SeamCarver
 		return result;
 	}
 	
+	private class Node
+	{	 
+		private int x;
+		private int y;
+		public Node(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+		public int x()
+		{
+			return this.x;
+		}
+		public int y()
+		{
+			return this.y;
+		}
+		
+	}
 	
 	private int[] seam()
 	{
 		
 		double[][] energy=new double[width()][height()];
 		double[][]distTo= new double[width()][height()];
-		int[] prev= new int[(height()+1)*(width()+1)];
+		Node[][] prev= new Node[width()][height()];
 		for(int i=0; i<width(); i++)
 		{
 			for(int j=0; j<height(); j++)
 			{
 				energy[i][j]=energy(i,j);
 				distTo[i][j]=Double.POSITIVE_INFINITY;
+				prev[i][j]=new Node(-1,-1);
 			}
 		}
 		for(int i=0; i<width(); i++)
 		{
 			distTo[i][0]=0.0;
 		}
-		for(int j=0; j<(height()+1)*(width()+1); j++)
-		{
-			prev[j]=-1;
-		}
 		
 		int upper=-1;
 		int lower=2;
-		int end=0;
 		for(int j=0; j<height()-1; j++)
 		{
 			for(int i=0; i<width(); i++)
@@ -101,38 +117,42 @@ public class SeamCarver
 					if(distTo[i+k][j+1]>newdis)
 					{
 						distTo[i+k][j+1]= newdis;
-						prev[width()*(i+k)+(j+1)]=width()*i+j;
+						prev[i+k][j+1]= new Node(i,j);
 					}
 					
 				}
 				
 			}
 		}
-		double dis=distTo[0][height()-1];
-		int a=0;
+		Node a=new Node(0,0);
+		double temp=distTo[0][height()-1];
 		for(int i=1; i<width(); i++)
 		{
-			if(distTo[i][height()-1]<dis)
+			if(temp>distTo[i][height()-1])
 			{
-				a=i;
-				dis=distTo[i][height()-1];
+				temp=distTo[i][height()-1];
+				a=new Node(i,height()-1);
 			}
 		}
-		Stack<Integer> t= new Stack<Integer>();
-		int cur=a*width()+height()-1;
-		while(prev[cur]!=-1)
+		
+		Stack<Integer> help= new Stack<Integer>();
+		
+		
+		while(prev[a.x()][a.y()].x()!=-1)
 		{
-			t.push((cur-cur%width())/width());
-			cur= prev[cur];
+			help.push(a.x());
+
+			a= prev[a.x()][a.y()];
 		}
+		
 		int[] result= new int[height()];
-		for(int i=1; i<height(); i++)
+		for(int i=1; i<result.length; i++)
 		{
-			if(t.isEmpty())
+			if(help.isEmpty())
 			{
 				break;
 			}
-			result[i]=t.pop();
+			result[i]=help.pop();
 		}
 		if(result[1]==0)
 		{
@@ -142,7 +162,7 @@ public class SeamCarver
 		{
 			result[0]=width()-2;
 		}
-		if(result[1]!=width()-1&&result[1]!=0)
+		if(result[1]!=0&&result[1]!=width()-1)
 		{
 			result[0]=result[1]-1;
 		}
@@ -170,6 +190,7 @@ public class SeamCarver
 		return result;
 	}
 
+	
 	public int[] findVerticalSeam()
 	{
 		return seam();
